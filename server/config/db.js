6a3ea30a0d0a74e24d3eceb1bc0f954eab0739c8
Mike Pham <mikephse@gmail.com>
2020-10-20 17:26:53 -0400

    fix connection to MongoDB 4th attempt

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,1 @@
-module.exports.URL ="mongodb+srv://theskynetz:Rosa2745@mongodbserver.fpisu.mongodb.net/book_store?retryWrites=true&w=majority"
+module.exports.URL = "mongodb+srv://theskynetz:Rosa2745@mongodbserver.fpisu.mongodb.net/book_store?retryWrites=true&w=majority";
